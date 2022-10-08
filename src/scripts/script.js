ca2865ee9ceb8a0360dbd2830d10b81714689aeb
chencsi <kevinkeeeh@gmail.com>
2022-10-09 01:48:07 +0200

    megallok megjelenitese

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -37,13 +37,16 @@ async function init(){ //main function
     document.querySelector(".bus-number").innerText = lastPart;
     document.querySelector(".time").innerText = time;
     stopsCount = stops;
+    $("ul li:nth-last-of-type(16) ~ li").hide();
 }
 
 init();
 
 document.getElementById("next-stop").onclick = ()=>{
+    $("li:nth-child(-n+" + (currentStop + 3) + ")").show();
     $("li:nth-child(-n+" + currentStop + ")").hide();
     currentStop += 1;
+    $("li:nth-child(-n+" + currentStop + ")").css("font-weight", "bold");
 };
 
 setTimeout(() => {
